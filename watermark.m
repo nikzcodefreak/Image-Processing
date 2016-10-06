a1a53295cf8e7cf8ed0045d846103612b77a9e48
Nikhil Damodaran <nikhildamodaran413@gmail.com>
2016-10-06 21:22:47 +0530

    added watermark code

---
+++ watermark.m
@@ -0,0 +1,15 @@
+I1=imread('cameraman.tif');
+I2=imread('rice.png');
+I1=double(I1);
+I2=double(I2);
+[u s v]=svd(I1);
+s1=s+0.2*I2;
+I3=u*s1*v';
+figure
+subplot(121)
+imshow(uint8(I3));
+title('water marked image');
+s2=(s1-s)/0.2;
+subplot(122);
+imshow(uint8(s2));
+title('Hidden image');
